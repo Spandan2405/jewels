66bf436dedb61b9d649766e31b8d3d1835737abd
Spandan2405 <spandan2405@gmail.com>
2025-03-21 19:49:59 +0530

    Deploying on Netlify

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,11 +1,17 @@
 import imageUrlBuilder from "@sanity/image-url";
 import { createClient } from "@sanity/client";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const useCdn = "true";
+
+// Sanity client configuration
 export const client = createClient({
-  projectId: "61wdzubq",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false,
+  projectId: projectId,
+  dataset: dataset,
+  apiVersion: apiVersion,
+  useCdn: useCdn,
 });
 
 const builder = imageUrlBuilder(client);
